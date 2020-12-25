855d15bcd4415fd4ef86a4bb6b770707df46f741
Loxymondor <mikosimo0@gmail.com>
2020-12-25 18:04:22 +0200

    pre-start

--- TSPsolution.hs
+++ TSPsolution.hs
@@ -2,10 +2,22 @@ module TSPsolution (solve,showSolution) where
 
 import qualified CombinatorialOptimisation.TSP as TSP
 showSolution :: TSP.TSPProblem -> String 
-showSolution a = show (solve a)
+showSolution a = show (solve a [0..((TSP.numCities a)-1)] 0 )
 
+kick :: (Eq a) => a -> [a] -> [a]
+kick n xs = filter (/=n) xs
 
--- solve :: TSP.TSPProblem -> [Floating a]
-solve i =  
-      where findMin  = (maximum).(map (TSP.edgeCost i 0)) [0..((TSP.numCities i)-1)] 
+solve :: TSP.TSPProblem  -> [Int] -> Int -> [Int]
+
+solve _ _ (-1) = [] 
+
+solve problem unvisted startpoint = [startpoint ] ++ (solve problem (kick startpoint unvisted) (nextMin problem startpoint unvisted)) 
+      where nextMin p s uv = head ( filter (==s+1) uv )
+             where head [] = -1
+                   head a  = Prelude.head a  
+
+-- ((maximum).(map (TSP.edgeCost p s))) uv 
+
+solve _ [] _ = [] 
+
 -- solve i = ((maximum).(TSP.edgeCost i 0)) [0..(TSP.numCities-1)] 
