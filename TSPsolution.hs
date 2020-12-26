4142910ceeaa3cfb1c5fff28e684981cc541338c
Loxymondor <mikosimo0@gmail.com>
2020-12-26 16:22:32 +0200

    works with pretty printing . correctness unchecked

--- TSPsolution.hs
+++ TSPsolution.hs
@@ -1,33 +1,39 @@
 module TSPsolution (solve,showSolution) where
 
 import qualified CombinatorialOptimisation.TSP as TSP
-import qualified Data.List as DL (foldl') 
+import qualified Data.List as DL (foldl',unlines) 
 
 showSolution :: TSP.TSPProblem -> String 
-showSolution a = show (solve a [1..((TSP.numCities a)-1)] 0 )
+showSolution a = (DL.unlines . (map show2T)) solution
+  where solution = (solve a [1..((TSP.numCities a)-1)] 0 )
+        show2T (a,b) = (show a) ++ "\t" ++ (show b) 
 
 kick :: (Eq a) => a -> [a] -> [a]
 kick n xs = filter (/=n) xs
 
 nextMin :: TSP.TSPProblem -> Int -> [Int] -> Int
-nextMin tsp sp uv = fst ( DL.foldl' minWeight (sp,inf) uv) 
+nextMin tsp sp uv = fst ( DL.foldl' minWeight (sp,inf) uv') 
    where 
 --           minWeight ::  Floating a =>  (Ord (Int -> a ))  => (Int,Int -> a) -> Int -> (Int,Int -> a)
-         inf=9999
+         inf = 9999
+         uv' = kick sp uv
          minWeight a@(_,w) b       | w < w'    = a
                                    | otherwise = b'
           where w' = TSP.edgeCost tsp sp b
                 b' = (b,w') 
+-- nextMin _ _ [] = 
 
-
-solve :: TSP.TSPProblem  -> [Int] -> Int -> [Int]
+solve :: Floating a => TSP.TSPProblem  -> [Int] -> Int -> [(Int,a)]
 
 solve _ [] _   = [] 
 
-solve problem unvisted startpoint = [startpoint ] ++ (solve problem unvisted' startpoint' ) 
+solve problem unvisted startpoint = [(startpoint, costOfNextStep) ] 
+                                    ++
+                                    (solve problem unvisted' startpoint' ) 
       where 
             startpoint' = (nextMin problem startpoint unvisted)
             unvisted' = (kick startpoint unvisted) 
+            costOfNextStep = TSP.edgeCost problem startpoint startpoint'
 --             unvisted' = kick startpoint' (kick startpoint unvisted) 
 -- ((maximum).(map (TSP.edgeCost p s))) uv 
 
