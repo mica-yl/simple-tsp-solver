d24cd68e7249f90ce6277920ca783738ad7a9ac9
Loxymondor <mikosimo0@gmail.com>
2020-12-26 15:29:06 +0200

    works with a solution but solution not tested

--- Main.hs
+++ Main.hs
@@ -4,7 +4,7 @@ import qualified TSPsolution as S
 
 -- read :: IO a -> IO b
 main = do 
-   let file = "/home/loxymondor/docs/facu/4th/sem1/AI/proj/TSP-Problems/problem1.tsp" 
+   let file = "/home/loxymondor/docs/facu/4th/sem1/AI/proj/TSP-Problems/problem4.atsp" 
    x <- FF.loadTSPFile TSP.ExplicitMatrix file
    putStr (showSolution x)
       where showSolution = S.showSolution 
--- TSPsolution.hs
+++ TSPsolution.hs
@@ -1,23 +1,35 @@
 module TSPsolution (solve,showSolution) where
 
 import qualified CombinatorialOptimisation.TSP as TSP
+import qualified Data.List as DL (foldl') 
+
 showSolution :: TSP.TSPProblem -> String 
-showSolution a = show (solve a [0..((TSP.numCities a)-1)] 0 )
+showSolution a = show (solve a [1..((TSP.numCities a)-1)] 0 )
 
 kick :: (Eq a) => a -> [a] -> [a]
 kick n xs = filter (/=n) xs
 
+nextMin :: TSP.TSPProblem -> Int -> [Int] -> Int
+nextMin tsp sp uv = fst ( DL.foldl' minWeight (sp,inf) uv) 
+   where 
+--           minWeight ::  Floating a =>  (Ord (Int -> a ))  => (Int,Int -> a) -> Int -> (Int,Int -> a)
+         inf=9999
+         minWeight a@(_,w) b       | w < w'    = a
+                                   | otherwise = b'
+          where w' = TSP.edgeCost tsp sp b
+                b' = (b,w') 
+
+
 solve :: TSP.TSPProblem  -> [Int] -> Int -> [Int]
 
-solve _ _ (-1) = [] 
+solve _ [] _   = [] 
 
-solve problem unvisted startpoint = [startpoint ] ++ (solve problem (kick startpoint unvisted) (nextMin problem startpoint unvisted)) 
-      where nextMin p s uv = head ( filter (==s+1) uv )
-             where head [] = -1
-                   head a  = Prelude.head a  
-
+solve problem unvisted startpoint = [startpoint ] ++ (solve problem unvisted' startpoint' ) 
+      where 
+            startpoint' = (nextMin problem startpoint unvisted)
+            unvisted' = (kick startpoint unvisted) 
+--             unvisted' = kick startpoint' (kick startpoint unvisted) 
 -- ((maximum).(map (TSP.edgeCost p s))) uv 
 
-solve _ [] _ = [] 
 
 -- solve i = ((maximum).(TSP.edgeCost i 0)) [0..(TSP.numCities-1)] 
